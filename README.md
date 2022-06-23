408598bc92310fe829ffd43b90a4b4d96f9e575a
ginabeki <ginabeki3@gmail.com>
2022-06-23 11:27:01 +0300

    intro

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction to javascript
+
+- Write a javascript code that prints out your name, height, and country on the screen
---
+++ script.js
@@ -0,0 +1,7 @@
+const firstName = document.getElementById("firstName");
+const height = document.getElementById("height");
+const country = document.getElementById("country");
+
+firstName.innerHTML = "Name: Gina";
+country.innerHTML = "Country: Kenya";
+height.innerHTML = "Height: 5 ft 5";
